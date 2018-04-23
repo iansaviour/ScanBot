c4e12be18dc52f1fd7f94e14278ffb9eab7fd07e
Putu Septian Primadewa <iansaviour@gmail.com>
2018-04-23 16:28:52 +0800

    Initialization

---
+++ ScanBot/ScanBot/ScanBotMain.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ScanBotMain
+    Inherits DevExpress.XtraEditors.XtraForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
+        Me.BStartScan = New DevExpress.XtraEditors.SimpleButton()
+        Me.PanelControl2 = New DevExpress.XtraEditors.PanelControl()
+        Me.LabelControl2 = New DevExpress.XtraEditors.LabelControl()
+        Me.TEDelay = New DevExpress.XtraEditors.TextEdit()
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        Me.TextEdit1 = New DevExpress.XtraEditors.TextEdit()
+        Me.PanelControl3 = New DevExpress.XtraEditors.PanelControl()
+        Me.BImportList = New DevExpress.XtraEditors.SimpleButton()
+        Me.PanelControl4 = New DevExpress.XtraEditors.PanelControl()
+        Me.GCScanList = New DevExpress.XtraGrid.GridControl()
+        Me.GVScanList = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GridColumnID = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumnCodeList = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumnCheckList = New DevExpress.XtraGrid.Columns.GridColumn()
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl1.SuspendLayout()
+        CType(Me.PanelControl2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl2.SuspendLayout()
+        CType(Me.TEDelay.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PanelControl3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl3.SuspendLayout()
+        CType(Me.PanelControl4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PanelControl4.SuspendLayout()
+        CType(Me.GCScanList, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GVScanList, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PanelControl1
+        '
+        Me.PanelControl1.Controls.Add(Me.BStartScan)
+        Me.PanelControl1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.PanelControl1.Location = New System.Drawing.Point(0, 479)
+        Me.PanelControl1.Name = "PanelControl1"
+        Me.PanelControl1.Size = New System.Drawing.Size(492, 32)
+        Me.PanelControl1.TabIndex = 0
+        '
+        'BStartScan
+        '
+        Me.BStartScan.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.BStartScan.Location = New System.Drawing.Point(2, 2)
+        Me.BStartScan.Name = "BStartScan"
+        Me.BStartScan.Size = New System.Drawing.Size(488, 28)
+        Me.BStartScan.TabIndex = 0
+        Me.BStartScan.Text = "Start Scan"
+        '
+        'PanelControl2
+        '
+        Me.PanelControl2.Controls.Add(Me.LabelControl2)
+        Me.PanelControl2.Controls.Add(Me.TEDelay)
+        Me.PanelControl2.Controls.Add(Me.LabelControl1)
+        Me.PanelControl2.Controls.Add(Me.TextEdit1)
+        Me.PanelControl2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelControl2.Location = New System.Drawing.Point(0, 0)
+        Me.PanelControl2.Name = "PanelControl2"
+        Me.PanelControl2.Size = New System.Drawing.Size(492, 69)
+        Me.PanelControl2.TabIndex = 1
+        '
+        'LabelControl2
+        '
+        Me.LabelControl2.Location = New System.Drawing.Point(12, 38)
+        Me.LabelControl2.Name = "LabelControl2"
+        Me.LabelControl2.Size = New System.Drawing.Size(51, 13)
+        Me.LabelControl2.TabIndex = 3
+        Me.LabelControl2.Text = "Delay (ms)"
+        '
+        'TEDelay
+        '
+        Me.TEDelay.Location = New System.Drawing.Point(86, 35)
+        Me.TEDelay.Name = "TEDelay"
+        Me.TEDelay.Size = New System.Drawing.Size(394, 20)
+        Me.TEDelay.TabIndex = 2
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(12, 12)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(68, 13)
+        Me.LabelControl1.TabIndex = 1
+        Me.LabelControl1.Text = "Window Name"
+        '
+        'TextEdit1
+        '
+        Me.TextEdit1.Location = New System.Drawing.Point(86, 9)
+        Me.TextEdit1.Name = "TextEdit1"
+        Me.TextEdit1.Size = New System.Drawing.Size(394, 20)
+        Me.TextEdit1.TabIndex = 0
+        '
+        'PanelControl3
+        '
+        Me.PanelControl3.Controls.Add(Me.BImportList)
+        Me.PanelControl3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelControl3.Location = New System.Drawing.Point(0, 69)
+        Me.PanelControl3.Name = "PanelControl3"
+        Me.PanelControl3.Size = New System.Drawing.Size(492, 38)
+        Me.PanelControl3.TabIndex = 2
+        '
+        'BImportList
+        '
+        Me.BImportList.Dock = System.Windows.Forms.DockStyle.Right
+        Me.BImportList.Location = New System.Drawing.Point(383, 2)
+        Me.BImportList.Name = "BImportList"
+        Me.BImportList.Size = New System.Drawing.Size(107, 34)
+        Me.BImportList.TabIndex = 0
+        Me.BImportList.Text = "Import List"
+        '
+        'PanelControl4
+        '
+        Me.PanelControl4.Controls.Add(Me.GCScanList)
+        Me.PanelControl4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PanelControl4.Location = New System.Drawing.Point(0, 107)
+        Me.PanelControl4.Name = "PanelControl4"
+        Me.PanelControl4.Size = New System.Drawing.Size(492, 372)
+        Me.PanelControl4.TabIndex = 3
+        '
+        'GCScanList
+        '
+        Me.GCScanList.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GCScanList.Location = New System.Drawing.Point(2, 2)
+        Me.GCScanList.MainView = Me.GVScanList
+        Me.GCScanList.Name = "GCScanList"
+        Me.GCScanList.Size = New System.Drawing.Size(488, 368)
+        Me.GCScanList.TabIndex = 0
+        Me.GCScanList.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GVScanList})
+        '
+        'GVScanList
+        '
+        Me.GVScanList.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumnID, Me.GridColumnCodeList, Me.GridColumnCheckList})
+        Me.GVScanList.GridControl = Me.GCScanList
+        Me.GVScanList.Name = "GVScanList"
+        Me.GVScanList.OptionsView.ShowGroupPanel = False
+        '
+        'GridColumnID
+        '
+        Me.GridColumnID.Caption = "ID"
+        Me.GridColumnID.FieldName = "id"
+        Me.GridColumnID.Name = "GridColumnID"
+        '
+        'GridColumnCodeList
+        '
+        Me.GridColumnCodeList.Caption = "Code List"
+        Me.GridColumnCodeList.FieldName = "code"
+        Me.GridColumnCodeList.Name = "GridColumnCodeList"
+        Me.GridColumnCodeList.Visible = True
+        Me.GridColumnCodeList.VisibleIndex = 0
+        Me.GridColumnCodeList.Width = 1378
+        '
+        'GridColumnCheckList
+        '
+        Me.GridColumnCheckList.AppearanceCell.Options.UseTextOptions = True
+        Me.GridColumnCheckList.AppearanceCell.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center
+        Me.GridColumnCheckList.AppearanceHeader.Options.UseTextOptions = True
+        Me.GridColumnCheckList.AppearanceHeader.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center
+        Me.GridColumnCheckList.Caption = "CheckList"
+        Me.GridColumnCheckList.FieldName = "is_check"
+        Me.GridColumnCheckList.Name = "GridColumnCheckList"
+        Me.GridColumnCheckList.Visible = True
+        Me.GridColumnCheckList.VisibleIndex = 1
+        Me.GridColumnCheckList.Width = 254
+        '
+        'ScanBotMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(492, 511)
+        Me.Controls.Add(Me.PanelControl4)
+        Me.Controls.Add(Me.PanelControl3)
+        Me.Controls.Add(Me.PanelControl2)
+        Me.Controls.Add(Me.PanelControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MinimizeBox = False
+        Me.Name = "ScanBotMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Scan Bot"
+        CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl1.ResumeLayout(False)
+        CType(Me.PanelControl2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl2.ResumeLayout(False)
+        Me.PanelControl2.PerformLayout()
+        CType(Me.TEDelay.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TextEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PanelControl3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl3.ResumeLayout(False)
+        CType(Me.PanelControl4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PanelControl4.ResumeLayout(False)
+        CType(Me.GCScanList, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GVScanList, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PanelControl1 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents PanelControl2 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents PanelControl3 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents PanelControl4 As DevExpress.XtraEditors.PanelControl
+    Friend WithEvents GCScanList As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GVScanList As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GridColumnID As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumnCodeList As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumnCheckList As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents BStartScan As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents BImportList As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents TextEdit1 As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents LabelControl2 As DevExpress.XtraEditors.LabelControl
+    Friend WithEvents TEDelay As DevExpress.XtraEditors.TextEdit
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+End Class
---
+++ ScanBot/ScanBot/ScanBotMain.vb
@@ -0,0 +1,178 @@
+﻿Imports System.Data.OleDb
+Imports System.Runtime.InteropServices
+Imports System.Threading
+
+Public Class ScanBotMain
+    '<DllImport("User32.dll")> Private Shared Function SetForegroundWindow(ByVal point As IntPtr) As Integer
+    'End Function
+
+    'Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles SimpleButton1.Click
+    '    Dim proc_name As String = TextEdit1.Text
+    '    Dim p As Process = Process.GetProcessesByName(proc_name).FirstOrDefault()
+    '    If Not IsNothing(p) Then
+    '        Dim h As IntPtr = p.MainWindowHandle
+    '        SetForegroundWindow(h)
+    '        SendKeys.SendWait(TextEdit2.Text)
+    '        Console.WriteLine("ok")
+    '    End If
+    'End Sub
+    Private Declare Sub keybd_event Lib "user32" (ByVal bVk As Byte, ByVal bScan As Byte, ByVal dwFlags As UInteger, ByVal dwExtraInfo As UInteger)
+    Const KEYEVENTF_KEYUP = &H2
+    Private Declare Function ShowWindow Lib "user32.dll" (ByVal hWnd As IntPtr, ByVal nCmdShow As SHOW_WINDOW) As Boolean
+    Private Declare Function FindWindow Lib "user32" Alias "FindWindowA" (ByVal zero As IntPtr, ByVal lpWindowName As String) As IntPtr
+    Private Declare Function SetForegroundWindow Lib "user32" (ByVal hwnd As IntPtr) As Integer
+    Private Declare Function GetForegroundWindow Lib "user32" () As Integer
+
+    Private Enum SHOW_WINDOW As Integer
+        SW_HIDE = 0
+        SW_SHOWNORMAL = 1
+        SW_NORMAL = 1
+        SW_SHOWMINIMIZED = 2
+        SW_SHOWMAXIMIZED = 3
+        SW_MAXIMIZE = 3
+        SW_SHOWNOACTIVATE = 4
+        SW_SHOW = 5
+        SW_MINIMIZE = 6
+        SW_SHOWMINNOACTIVE = 7
+        SW_SHOWNA = 8
+        SW_RESTORE = 9
+        SW_SHOWDEFAULT = 10
+        SW_FORCEMINIMIZE = 11
+        SW_MAX = 11
+    End Enum
+
+
+    <DllImport("user32.dll", SetLastError:=True)>
+    Friend Shared Function SendInput(ByVal cInputs As Int32, ByRef pInputs As INPUT, ByVal cbSize As Int32) As Int32
+    End Function
+
+    <StructLayout(LayoutKind.Explicit, Pack:=1, Size:=28)>
+    Friend Structure INPUT
+        <FieldOffset(0)> Public dwType As InputType
+        <FieldOffset(4)> Public ki As KEYBDINPUT
+    End Structure
+
+    <StructLayout(LayoutKind.Sequential, Pack:=1)>
+    Friend Structure KEYBDINPUT
+        Public wVk As Int16
+        Public wScan As Int16
+        Public dwFlags As KEYEVENTF
+        Public time As Int32
+        Public dwExtraInfo As IntPtr
+    End Structure
+
+    Friend Enum InputType As Integer
+        Keyboard = 1
+    End Enum
+
+    <Flags()>
+    Friend Enum KEYEVENTF As Integer
+        EXTENDEDKEY = 1
+        KEYUP = 2
+        [UNICODE] = 4
+        SCANCODE = 8
+    End Enum
+
+    Private Sub SendTheKeys(ByVal flags As KEYEVENTF, ByVal key As Keys)
+        Dim input As New INPUT
+        Dim ki As New KEYBDINPUT
+        input.dwType = InputType.Keyboard
+        input.ki = ki
+        input.ki.wVk = Convert.ToInt16(key)
+        input.ki.wScan = 0
+        input.ki.time = 0
+        input.ki.dwFlags = flags
+        input.ki.dwExtraInfo = IntPtr.Zero
+        Dim cbSize As Integer = Marshal.SizeOf(GetType(INPUT))
+        Dim result As Integer = SendInput(1, input, cbSize)
+        If result = 0 Then Debug.WriteLine(Marshal.GetLastWin32Error)
+    End Sub
+
+    Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles BStartScan.Click
+        Dim window_name As String = TextEdit1.Text
+        Dim targetwindow As IntPtr = FindWindow(Nothing, window_name)
+        SetForegroundWindow(targetwindow)
+        For i As Integer = 0 To GVScanList.RowCount - 1
+            SendKeys.Send(GVScanList.GetRowCellValue(i, "code").ToString)
+            SendKeys.Send("{enter}")
+            'delay
+            Thread.Sleep(TEDelay.EditValue)
+            'check if error
+            Dim windowhandle As IntPtr = FindWindow(Nothing, "Stop")
+            If windowhandle = Nothing Then
+                GVScanList.SetRowCellValue(i, "is_check", "yes")
+            Else
+                Exit For
+            End If
+        Next
+        GVScanList.RefreshData()
+    End Sub
+
+    Private Sub SimpleButton2_Click(sender As Object, e As EventArgs) Handles BImportList.Click
+        'import excel
+        load_excel_data()
+        '
+        'Trial
+        'Dim dt As New DataTable
+        'dt.Columns.Add("ID")
+        'dt.Columns.Add("code")
+        'dt.Columns.Add("is_check")
+        'dt.Columns(0).AutoIncrement = True
+        '
+
+        'For i As Integer = 0 To 10
+        '    Dim R1 As DataRow = dt.NewRow
+        '    R1("code") = "888820092991"
+        '    R1("is_check") = "no"
+        '    dt.Rows.Add(R1)
+        'Next
+        ''
+        'Dim R3 As DataRow = dt.NewRow
+        'R3("ID") = "3"
+        'R3("code") = "888820092992"
+        'R3("is_check") = "no"
+        'dt.Rows.Add(R3)
+
+        'For i As Integer = 0 To 10
+        '    Dim R1 As DataRow = dt.NewRow
+        '    R1("code") = "888820092991"
+        '    R1("is_check") = "no"
+        '    dt.Rows.Add(R1)
+        'Next
+        'GCScanList.DataSource = dt
+        'GCScanList.Refresh()
+        'GVScanList.RefreshData()
+        ''
+
+        '
+
+    End Sub
+
+    Private Sub ScanBotMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TEDelay.EditValue = 100
+    End Sub
+
+    Sub load_excel_data()
+        Dim oledbconn As New OleDbConnection
+        Dim strConn As String
+        Dim data_temp As New DataTable
+
+        Dim path As String = My.Application.Info.DirectoryPath.ToString & "\code_import.xlsx"
+        Console.WriteLine(path)
+
+        strConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source='" & path & "';Extended Properties=""Excel 12.0 XML; IMEX=1;HDR=NO;TypeGuessRows=0;ImportMixedTypes=Text;"""
+        oledbconn.ConnectionString = strConn
+        Dim MyCommand As OleDbDataAdapter
+        Try
+            MyCommand = New OleDbDataAdapter("select [F1] as index,[F2] as code,'no' as is_check from [code$] WHERE NOT [F2] IS NULL", oledbconn)
+            MyCommand.Fill(data_temp)
+            MyCommand.Dispose()
+        Catch ex As Exception
+            MyCommand = New OleDbDataAdapter("select [F1] as index,[F2] as code,'no' as is_check from [code$] WHERE NOT [F2] IS NULL", oledbconn)
+            MyCommand.Fill(data_temp)
+            MyCommand.Dispose()
+        End Try
+
+        GCScanList.DataSource = data_temp
+    End Sub
+End Class
